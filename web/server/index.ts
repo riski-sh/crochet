941ab7e40534c90b2c475deceed5c149d583695a
vpapandrea <wsh@riski.sh>
2020-09-14 13:52:02 -0400

    added basic static file server

--- web/server/index.ts
+++ web/server/index.ts
@@ -1,9 +1,8 @@
 import express = require('express');
 
 const app: express.Application = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+
+app.use('/', express.static('html'))
 
 app.listen(3000, function () {
   console.log('App is listening on port 3000!');
